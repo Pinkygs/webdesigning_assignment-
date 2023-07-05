ef9c6ef1848e1a171a828994182083276d082f3b
Pinkygs <pinkygs364@gmail.com>
2023-07-05 09:32:16 +0530

    commit all file

---
+++ Module 2/html/js/array1.js
@@ -0,0 +1,9 @@
+let nos = [1,2,3,4,5];
+console.log("All element of the array are:",nos);
+//conslose.log(nos);
+//length of the array
+console.log("length of the array are:",nos.length);
+//print the first element
+console.log("The First element is:",nos[0]);
+//print last element
+console.log("Last element is:",nos[nos.length-1]);
---
+++ Module 2/html/js/attay3.js
@@ -0,0 +1,18 @@
+let nos =[1,2,3,4,5];
+console.log("Initially nos are:",nos);
+nos.shift(2);
+console.log("Nos after shift:",nos);
+nos.unshift(1);
+console.log("Nos after unshift:",nos);
+nos.splice(0,3)
+console.log("Nos after splice deletion:",nos)
+//insert element in between using slipce
+nos.splice(2,2,7,8);
+console.log("Nos after splice addition",nos);
+f = ["a","b","c"];
+console.log(f);
+f.splice(1,0,"d","e","f");
+console.log("Values after addition",f);
+g = ["x","y","z"]
+console.log(g);
+f.splice(1,0)
